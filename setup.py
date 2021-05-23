1af42c6663b8afec6ec9c175d4256003992d6520
Ben Greene <BenJGreene@gmail.com>
2021-05-23 17:18:54 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from setuptools_rust import RustExtension, Binding
+
+
+setup(
+    name="setuptools-rust-starter",
+    version="0.1.0",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Rust",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+    ],
+    packages=["setuptools_rust_starter"],
+    rust_extensions=[
+        RustExtension("setuptools_rust_starter._setuptools_rust_starter", binding=Binding.PyO3)
+        ],
+    include_package_data=True,
+    zip_safe=False,
+)
