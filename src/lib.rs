562724d712360a6d7d15ce72b29843af502a7329
Ben Greene <Ben.Greene@digitalreasoning.com>
2021-07-13 20:57:52 +0100

    Update to git branch of pyo3-asyncio

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
-use pyo3::prelude::*;
 use pyo3::types::PyDict;
-use pyo3::wrap_pymodule;
-
 mod submodule;
 use submodule::*;
+use std::time::Duration;
+use async_std::task;
+// use pyo3_asyncio::async_std::into_coroutine;
+
+use pyo3::{prelude::*, wrap_pyfunction, wrap_pymodule};
+
 
 #[pyclass]
 struct ExampleClass {
@@ -27,14 +30,49 @@ impl ExampleClass {
     }
 }
 
+
+// async fn rust_sleep() {
+//     async_std::task::sleep(Duration::from_secs(1)).await;
+// }
+
+#[pyfunction]
+fn foo<'p>(py: Python<'p>) -> PyResult<&'p PyAny> {
+    pyo3_asyncio::async_std::future_into_py(py, async move {
+        task::sleep(Duration::from_secs(1)).await;
+        Python::with_gil(|py: Python| Ok(py.None()))
+    })
+}
+
+#[pyfunction]
+fn sleep_for<'p>(py: Python<'p>, secs: &'p PyAny) -> PyResult<&'p PyAny> {
+    let secs = secs.extract()?;
+
+    pyo3_asyncio::async_std::future_into_py(py, async move {
+        task::sleep(Duration::from_secs(secs)).await;
+        Python::with_gil(|py| Ok(py.None()))
+    })
+}
+
+#[pyfunction]
+fn rust_sleep1<'p>(py: Python<'p>) -> PyResult<&'p PyAny> {
+    pyo3_asyncio::async_std::future_into_py(py, async {
+        async_std::task::sleep(std::time::Duration::from_secs(1)).await;
+        Ok(Python::with_gil(|py| py.None()))
+    })
+}
+
 #[pymodule]
 fn _setuptools_rust_starter(py: Python, m: &PyModule) -> PyResult<()> {
+    // pyo3_asyncio::try_init(py)?;
+
+    m.add_function(wrap_pyfunction!(foo, m)?)?;
+    m.add_function(wrap_pyfunction!(sleep_for, m)?)?;
+    m.add_function(wrap_pyfunction!(rust_sleep1, m)?)?;
     m.add_class::<ExampleClass>()?;
     m.add_wrapped(wrap_pymodule!(submodule))?;
 
     // Inserting to sys.modules allows importing submodules nicely from Python
     // e.g. from setuptools_rust_starter.submodule import SubmoduleClass
-
     let sys = PyModule::import(py, "sys")?;
     let sys_modules: &PyDict = sys.getattr("modules")?.downcast()?;
     sys_modules.set_item("setuptools_rust_starter.submodule", m.getattr("submodule")?)?;
